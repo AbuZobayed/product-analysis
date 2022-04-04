f5d4436cc41a300001c940c23f0c0e88c8401f51
AbuZobayed <zobayed.ict05@gmail.com>
2022-04-04 20:09:14 +0600

    lineChart added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,56 @@
 import React from 'react';
+import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import './Dashboard.css'
 
 const Dashboard = () => {
+    const data = [
+        {
+            "month": "Mar",
+            "price": 100000,
+            "sell": 241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "price": 200000,
+            "sell": 423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "price": 500000,
+            "sell": 726,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "price": 500000,
+            "sell": 529,
+            "revenue": 40405
+        },
+        {
+            "month": "Jul",
+            "price": 600000,
+            "sell": 601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "price": 700000,
+            "sell": 670,
+            "revenue": 61000
+        }
+    ]
     return (
-        <div>
-            <h4>This is Dashboarrd!!</h4>
+        <div className='line-chart'>
+            <LineChart width={400} height={500} data={data}>
+            <Line dataKey={'price'}></Line>
+            <Line dataKey={'sell'}></Line>
+            <Line dataKey={'revenue'}></Line>
+            <XAxis dataKey={'month'}></XAxis>
+            <Tooltip></Tooltip>
+            <YAxis></YAxis>
+        </LineChart>
         </div>
     );
 };
